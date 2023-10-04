2c0752a045a1edd21c4c0649c417a962ae3fece8
rcmilena <145223132+rcmilena@users.noreply.github.com>
2023-10-04 14:34:30 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -68,6 +68,18 @@ if resposta=="s":
     """]
     print(lista[0]) 
     print(tracinhos)
+while true:
+     print("tracinhos".join(palavra))
+        letra = input("Digite uma letra: ")
+
+        if letra in palavra:
+            for i in range(len(palavra)):
+                if palavra[i] == letra:
+                    palavra_oculta[i] = letra
+        else:
+            forca += 1
+            print(f"Errou! Forca: {forca}")
+    
         
   
 elif resposta=="n":
