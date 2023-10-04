57b5b502f055e2966d683510e0df91a03f6824d2
rcmilena <145223132+rcmilena@users.noreply.github.com>
2023-10-04 21:59:30 +0000

    forca.py

--- forca.py
+++ forca.py
@@ -1,76 +1,94 @@
 print("Jogo da forca")
 palavra="batata"
 tracinhos = "_ " * len(palavra)
-resposta=input("Quer jogar?")
+lista = ["""
++---+
+|   |
+    |
+    |
+    |
+    |
+=======
+""",
+"""
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========
+""",
+"""
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========
+""",
+"""
++---+
+|   |
+O   |
+/|   |
+    |
+    |
+=========
+""",
+"""
+ +---+
+ |   |
+ O   |
+/|\  |
+    |
+    |
+=========
+""",
+"""
++---+
+|   |
+O   |
+/|\  |
+/    |
+    |
+=========
+""",
+"""
++---+
+|   |
+O   |
+/|\  |
+/ \  |
+    |
+=========
+"""]
+resposta=input("Quer jogar? ")
 if resposta=="s":
-    lista = ["""
-    +---+
-    |   |
-        |
-        |
-        |
-        |
-    =======
-    """,
-    """
-    +---+
-    |   |
-    O   |
-        |
-        |
-        |
-    =========
-    """,
-    """
-    +---+
-    |   |
-    O   |
-    |   |
-        |
-        |
-    =========
-    """,
-    """
-    +---+
-    |   |
-    O   |
-    /|   |
-        |
-        |
-    =========
-    """,
-    """
-    +---+
-    |   |
-    O   |
-    /|\  |
-        |
-        |
-    =========
-    """,
-    """
-    +---+
-    |   |
-    O   |
-    /|\  |
-    /    |
-        |
-    =========
-    """,
-    """
-     +---+
-     |   |
-     O   |
-    /|\  |
-    / \  |
-         |
-    =========
-    """]
     print(lista[0]) 
     print(tracinhos)
-        
-  
-elif resposta=="n":
-    print("ok, até mais!")
-
-
+    vida = 7
+    while vida > 0:
+        acertou = False
+        escolha = input("escolha uma letra: ")
+        novo_tracinhos = ''
+        for i in range(len(palavra)):
+            if palavra[i] == escolha:  
+                novo_tracinhos += escolha
+                acertou = True
+            else:
+                novo_tracinhos += tracinhos[i]
+        tracinhos = novo_tracinhos
+        if acertou:
+            print(tracinhos)
+            if tracinhos == palavra:
+                print("Parabéns, você ganhou!")
+                break
+        else:
+            print("Você errou, menos uma vida.")
+            vida = vida - 1
+            print(lista[7-vida])
+    while 
+elif resposta == "n":
+    print("tchau")
